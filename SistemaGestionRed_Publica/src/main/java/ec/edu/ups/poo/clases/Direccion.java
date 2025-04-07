5d2628441448f9d72fdd461a6938ad0e923d9d65
Erika-colla <collaguazobelen8@gmail.com>
2025-04-07 13:04:03 -0500

    Cambio en persona y direccion, y creacion de constantes

--- SistemaGestionRed_Publica/src/main/java/ec/edu/ups/poo/clases/Direccion.java
+++ SistemaGestionRed_Publica/src/main/java/ec/edu/ups/poo/clases/Direccion.java
@@ -1,7 +1,9 @@
 package ec.edu.ups.poo.clases;
 
+import ec.edu.ups.poo.enums.TipoDireccion;
+
 public class Direccion {
-    private String tipo;
+    private TipoDireccion tipoDireccion;
     private String callePrincipal;
     private String calleSecundaria;
     private String numeracion;
@@ -9,60 +11,71 @@ public class Direccion {
     private String provincia;
     private String pais;
 
-    public String getTipo() {
-        return tipo;
+    public Direccion(String callePrincipal, String calleSecundaria, String numeracion, String ciudad, String provincia, String pais, TipoDireccion tipoDireccion) {
+        this.callePrincipal = callePrincipal;
+        this.calleSecundaria = calleSecundaria;
+        this.numeracion = numeracion;
+        this.ciudad = ciudad;
+        this.provincia = provincia;
+        this.pais = pais;
+        this.tipoDireccion = TipoDireccion.INSTITUCION;
     }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
     public String getCallePrincipal() {
         return callePrincipal;
     }
-
     public void setCallePrincipal(String callePrincipal) {
         this.callePrincipal = callePrincipal;
     }
-
     public String getCalleSecundaria() {
         return calleSecundaria;
     }
-
     public void setCalleSecundaria(String calleSecundaria) {
         this.calleSecundaria = calleSecundaria;
     }
-
     public String getNumeracion() {
         return numeracion;
     }
-
     public void setNumeracion(String numeracion) {
         this.numeracion = numeracion;
     }
-
     public String getCiudad() {
         return ciudad;
     }
-
     public void setCiudad(String ciudad) {
         this.ciudad = ciudad;
     }
-
     public String getProvincia() {
         return provincia;
     }
-
     public void setProvincia(String provincia) {
         this.provincia = provincia;
     }
-
     public String getPais() {
         return pais;
     }
-
     public void setPais(String pais) {
         this.pais = pais;
     }
-}
 
+    public TipoDireccion getTipoDireccion() {
+        return tipoDireccion;
+    }
+
+    public void setTipoDireccion(TipoDireccion tipoDireccion) {
+        this.tipoDireccion = tipoDireccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Direccion{" +
+                "callePrincipal='" + callePrincipal + '\'' +
+                ", calleSecundaria='" + calleSecundaria + '\'' +
+                ", numeracion='" + numeracion + '\'' +
+                ", ciudad='" + ciudad + '\'' +
+                ", provincia='" + provincia + '\'' +
+                ", pais='" + pais + '\'' +
+                '}';
+    }
+}
+ 
+ 
--- SistemaGestionRed_Publica/src/main/java/ec/edu/ups/poo/clases/Persona.java
+++ SistemaGestionRed_Publica/src/main/java/ec/edu/ups/poo/clases/Persona.java
@@ -10,52 +10,64 @@ public class Persona {
     private String correo;
     private List<Direccion> direcciones;
 
-    public String getCedula() {
-        return cedula;
+    public Persona() {
+        direcciones = new ArrayList<>();
     }
 
-    public void setCedula(String cedula) {
+    public Persona(String cedula, String nombre, String apellido, String telefono, String correo) {
         this.cedula = cedula;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
         this.apellido = apellido;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
         this.telefono = telefono;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
         this.correo = correo;
     }
+        public String getCedula() {
+            return cedula;
+        }
+        public void setCedula(String cedula) {
+            this.cedula = cedula;
+        }
+        public String getNombre() {
+            return nombre;
+        }
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public String getApellido() {
+            return apellido;
+        }
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
+        public String getTelefono() {
+            return telefono;
+        }
+        public void setTelefono(String telefono) {
+            this.telefono = telefono;
+        }
+        public String getCorreo() {
+            return correo;
+        }
+        public void setCorreo(String correo) {
+            this.correo = correo;
+        }
 
-    public List<Direccion> getDirecciones() {
-        return direcciones;
-    }
+        public void addDireccion(Direccion direccion){
+            direcciones.add(direccion);
+        }
+        public List<Direccion> getDirecciones() {
+            return direcciones;
+        }
 
-    public void setDirecciones(List<Direccion> direcciones) {
-        this.direcciones = direcciones;
+        @Override
+        public String toString() {
+            return "Persona{" +
+                    "cedula='" + cedula + '\'' +
+                    ", nombre='" + nombre + '\'' +
+                    ", apellido='" + apellido + '\'' +
+                    ", telefono='" + telefono + '\'' +
+                    ", correoElectronico='" + correo + '\'' +
+                    '}';
+        }
     }
-}
 
